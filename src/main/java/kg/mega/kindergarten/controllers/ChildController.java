0e8090b9deeaa4c2cd9ba9e69132c2ff1058fb88
EldarOmu <omurzakoveldar58@gmail.com>
2025-06-15 15:53:53 +0600

    add preauthorize annotation for any methods

--- src/main/java/kg/mega/kindergarten/controllers/ChildController.java
+++ src/main/java/kg/mega/kindergarten/controllers/ChildController.java
@@ -5,6 +5,7 @@ import kg.mega.kindergarten.models.dtos.ChildCreateDto;
 import kg.mega.kindergarten.models.dtos.ChildDto;
 import kg.mega.kindergarten.models.dtos.ChildUpdateDto;
 import kg.mega.kindergarten.services.ChildService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,29 +22,34 @@ public class ChildController implements CRUDOperations<ChildDto, ChildCreateDto,
 
     @Override
     @Operation(summary = "Создание ребенка", description = "Создаение ребенка и сохранение в базу данных")
+    @PreAuthorize("hasRole('ADMIN')")
     public ChildDto create(ChildCreateDto dto) {
         return childService.create(dto);
     }
 
     @Override
     @Operation(summary = "Найти ребенка", description = "Находим ребенка спомощью id из базы данных")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
     public ChildDto read(Long id) {
         return childService.getChild(id);
     }
 
     @Override
     @Operation(summary = "Найти всех детей", description = "Находим список всех детей спомощью Pageable")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<ChildDto> readAll(int page, int size) { return childService.getAll(page, size);
     }
 
     @Override
     @Operation(summary = "Обновить ребенка", description = "Обновляем ребенка спомощью id")
+    @PreAuthorize("hasRole('ADMIN')")
     public ChildDto update(Long id, ChildUpdateDto uDTO) {
         return childService.updateChild(id, uDTO);
     }
 
     @Override
     @Operation(summary = "Удаляем ребенка", description = "Удаляем ребенка спомощью изменения поля active на false")
+    @PreAuthorize("hasRole('ADMIN')")
     public boolean delete(Long id) {
         return childService.deleteChild(id);
     }
--- src/main/java/kg/mega/kindergarten/controllers/ChildGroupHistoryController.java
+++ src/main/java/kg/mega/kindergarten/controllers/ChildGroupHistoryController.java
@@ -1,13 +1,11 @@
 package kg.mega.kindergarten.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kg.mega.kindergarten.models.Child;
-import kg.mega.kindergarten.models.ChildGroupHistory;
 import kg.mega.kindergarten.models.dtos.ChildGroupHistoryCreateDto;
 import kg.mega.kindergarten.models.dtos.ChildGroupHistoryDto;
 import kg.mega.kindergarten.services.ChildGroupHistoryService;
-import kg.mega.kindergarten.services.ChildService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,17 +19,21 @@ public class ChildGroupHistoryController {
 
     @PostMapping("/create")
     @Operation(summary = "Создание групповой истории ребенка", description = "Создание групповой истории ребенка и сохранение в базу данных")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
     public ResponseEntity<?> createChildGroupHistory(@RequestBody ChildGroupHistoryCreateDto childGroupHistoryCreateDto) {
         return ResponseEntity.ok(childGroupHistoryService.create(childGroupHistoryCreateDto));
     }
 
     @GetMapping("/get-child-group-history/{childId}")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Найти группу историй ребенка", description = "Находим группу историй ребенка спомощью childId")
     public ResponseEntity<?> getChildGroupHistoryByChildId(@PathVariable Long childId) {
         return ResponseEntity.ok(childGroupHistoryService.getChildGroupHistory(childId));
     }
 
     @PutMapping("/end-current-child-history-group/{childId}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    @Operation(summary = "Закончить текущую историю пребывания ребенка в группе", description = "Заканчиваем текущую историю пребывания ребенка в группе обозначая дату в момент использования метода")
     public ResponseEntity<?> endCurrentChildGroupHistory(@PathVariable Long childId) {
         ChildGroupHistoryDto childGroupHistoryDto = childGroupHistoryService.endCurrentChildGroupHistory(childId);
         return ResponseEntity.ok(childGroupHistoryDto);
